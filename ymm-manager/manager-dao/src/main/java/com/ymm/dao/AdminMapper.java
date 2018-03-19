7a962d01705c37efb3568bbfe07ba489b10383d9
naunchazy <1476276902@qq.com>
2018-03-19 23:36:54 +0800

    jedis和spring的整合

--- ymm-manager/manager-dao/src/main/java/com/ymm/dao/AdminMapper.java
+++ ymm-manager/manager-dao/src/main/java/com/ymm/dao/AdminMapper.java
@@ -1,15 +1,15 @@
-package com.ymm.dao;
-
-import com.ymm.pojo.dto.Page;
-import com.ymm.pojo.po.Admin;
-import org.apache.ibatis.annotations.Param;
-
-import java.util.List;
-import java.util.Map;
-
-public interface AdminMapper {
-    Admin selectAdminByUsername(Admin admin);
-    Admin selectAdminById(int id);
-    List<Admin> selectAllAdmin(Map<String,Object> map);
-    int countAdmin(String adminQueryName);
-}
+//package com.ymm.dao;
+//
+//import com.ymm.pojo.dto.Page;
+//import com.ymm.pojo.po.Admin;
+//import org.apache.ibatis.annotations.Param;
+//
+//import java.util.List;
+//import java.util.Map;
+//
+//public interface AdminMapper {
+//    Admin selectAdminByUsername(Admin admin);
+//    Admin selectAdminById(int id);
+//    List<Admin> selectAllAdmin(Map<String,Object> map);
+//    int countAdmin(String adminQueryName);
+//}
--- ymm-manager/manager-service/src/main/java/com/ymm/service/impl/AdminServiceimpl.java
+++ ymm-manager/manager-service/src/main/java/com/ymm/service/impl/AdminServiceimpl.java
@@ -1,45 +1,45 @@
-package com.ymm.service.impl;
-
-import com.ymm.dao.AdminMapper;
-import com.ymm.pojo.dto.Page;
-import com.ymm.pojo.po.Admin;
-import com.ymm.service.AdminService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-@Service
-public class AdminServiceimpl implements AdminService {
-    @Autowired
-    AdminMapper adminMapper;
-
-    @Override
-    public Admin selectAdminByUsername(Admin admin) {
-        Admin admin1 = adminMapper.selectAdminByUsername(admin);
-        return admin1;
-    }
-
-    @Override
-    public Admin selectAdminById(int id) {
-        Admin admin = adminMapper.selectAdminById(id);
-        return admin;
-    }
-
-    @Override
-    public List<Admin> selectAllAdmin(Page page, String adminQueryName) {
-        Map<String,Object> map = new HashMap<>();
-        map.put("page",page);
-        map.put("adminQueryName",adminQueryName);
-        List<Admin> admins = adminMapper.selectAllAdmin(map);
-        return admins;
-    }
-
-    @Override
-    public int countAdmin(String adminQueryName) {
-        int count = adminMapper.countAdmin(adminQueryName);
-        return count;
-    }
-}
+//package com.ymm.service.impl;
+//
+//import com.ymm.dao.AdminMapper;
+//import com.ymm.pojo.dto.Page;
+//import com.ymm.pojo.po.Admin;
+//import com.ymm.service.AdminService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.HashMap;
+//import java.util.List;
+//import java.util.Map;
+//
+//@Service
+//public class AdminServiceimpl implements AdminService {
+//    @Autowired
+//    AdminMapper adminMapper;
+//
+//    @Override
+//    public Admin selectAdminByUsername(Admin admin) {
+//        Admin admin1 = adminMapper.selectAdminByUsername(admin);
+//        return admin1;
+//    }
+//
+//    @Override
+//    public Admin selectAdminById(int id) {
+//        Admin admin = adminMapper.selectAdminById(id);
+//        return admin;
+//    }
+//
+//    @Override
+//    public List<Admin> selectAllAdmin(Page page, String adminQueryName) {
+//        Map<String,Object> map = new HashMap<>();
+//        map.put("page",page);
+//        map.put("adminQueryName",adminQueryName);
+//        List<Admin> admins = adminMapper.selectAllAdmin(map);
+//        return admins;
+//    }
+//
+//    @Override
+//    public int countAdmin(String adminQueryName) {
+//        int count = adminMapper.countAdmin(adminQueryName);
+//        return count;
+//    }
+//}
