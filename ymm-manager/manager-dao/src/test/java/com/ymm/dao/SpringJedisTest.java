c7af127e46de6bc512cb79bbf55b31b4dc38e743
naunchazy <1476276902@qq.com>
2018-03-20 16:17:50 +0800

    Jedis和spring的整合

--- ymm-manager/manager-dao/src/test/java/com/ymm/dao/SpringJedisTest.java
+++ ymm-manager/manager-dao/src/test/java/com/ymm/dao/SpringJedisTest.java
@@ -1,10 +1,11 @@
+//package com.ymm.dao;
+//
 //import com.ymm.commons.jedis.JedisClient;
 //import org.junit.Test;
 //import org.junit.runner.RunWith;
 //import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.test.context.ContextConfiguration;
 //import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-//
 //
 //@RunWith(SpringJUnit4ClassRunner.class)
 //@ContextConfiguration("classpath:spring/spring-*.xml")
